b11f842141fd54723294b2c7ba507e5d659fd4e5
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-27 12:01:25 +0300

    feat: create gird square dynamically and add event Listeners to button in order user to set grid size

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const mainContainer = document.querySelector(".main-container");
+const askGridSizeBtn = document.querySelector("#ask");
 
-for (let i = 0; i < 256; i++) {
-  const box = document.createElement("div");
-  box.className = "box";
-  box.addEventListener("mouseover", (e) => {
-    const randomR = Math.floor(Math.random() * 256);
-    const randomG = Math.floor(Math.random() * 256);
-    const randomB = Math.floor(Math.random() * 256);
-    box.style.background = `rgb(${randomR}, ${randomG}, ${randomB})`;
-  });
-  mainContainer.appendChild(box);
+askGridSizeBtn.addEventListener("click", (e) => {
+  const gridSizePrompt = prompt(
+    "How many squares do you want on each side of the grid?"
+  );
+
+  if (!gridSizePrompt || gridSizePrompt > 100 || gridSizePrompt < 0) {
+    return console.log("please Enter Valid Number upto 100");
+  }
+  let gridSize = parseInt(gridSizePrompt * gridSizePrompt);
+  createGrid(gridSize);
+});
+
+function createGrid(size = 256) {
+  let gridWidth = 1240;
+  let calcSquareSize = gridWidth / Math.sqrt(size);
+  mainContainer.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    const box = document.createElement("div");
+    box.className = "box";
+    box.innerText = `${i + 1}`;
+    box.style.width = `${calcSquareSize}px`;
+    box.style.height = `${calcSquareSize}px`;
+    box.addEventListener("mouseover", (e) => {
+      const randomR = Math.floor(Math.random() * 256);
+      const randomG = Math.floor(Math.random() * 256);
+      const randomB = Math.floor(Math.random() * 256);
+      box.style.background = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    });
+    mainContainer.appendChild(box);
+  }
 }
+
+createGrid();
